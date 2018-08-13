c3915eea061eb3152d5bb2947090c8569b4d6445
Nandakishor.m <nandakishor.m@happiestminds.com>
2018-08-13 20:31:47 +0530

    Demo code for working with GET,POST,EJS-templates,Middlewares,Body-parser...

---
+++ ejsviewjs.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.set('view engine', 'ejs');
+
+app.get('/profile/:name', function(req, res) {
+    var otherData = {
+        job: "ninja", 
+        email: "nanda_is_a_ninja@gmail.com",
+        hobbies: ["eating", "fighting", "reading"]
+    }
+    res.render('profile', { person: req.params.name, data: otherData });
+});
+
+app.get('/contact', function(req, res) {
+    console.log(req.query);
+    res.render('contact');
+});
+
+app.post('/contact', urlencodedParser, function(req, res) {
+    console.log(req.body);
+    res.render('contact', {status: "success"});
+});
+
+app.listen(3000);
--- expressjs.js
+++ expressjs.js
@@ -6,8 +6,19 @@ app.get('/admin', function(req, res) {
     res.send("This is the admin page");
 });
 
-app.get('/profile/:id', function(req, res) {
-    res.send("You requested a profile with id: " + req.params.id);
+app.get('/profile/:name', function(req, res) {
+    res.send("You requested a profile with name: " + req.params.name);
+});
+
+app.get('/user', function(req, res) {
+    var pageElements = "<html><body><h2>" + 
+    "Welcome " + req.query.username +
+    "</h2><br>This is a demonstration of queryString(RequestParams) extraction</body></html>"
+    res.send(pageElements);
+});
+
+app.get('/getindexfile', function(req, res) {
+    res.sendFile(__dirname + "/resources/html/index.html");
 });
 
 app.listen(3000);
